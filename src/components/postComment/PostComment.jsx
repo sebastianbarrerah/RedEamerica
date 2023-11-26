4578adfdc8962887ce6f638c4002f70a3dab9863
Sebastian barrera <sebasbh2008@hotmail.com>
2023-11-26 18:41:34 -0500

    add home

--- src/components/postComment/PostComment.jsx
+++ src/components/postComment/PostComment.jsx
@@ -1,28 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './postComment.scss';
-import { useLocation } from 'react-router';
-const PostComment = ({dataPost}) => {
-    console.log(dataPost, "esto es de children");
-    // const [dataUser1, setDataUser1] = useState()
-    // const [dataPost1, setDataPost1] = useState()
-    // const location = useLocation()
-    // useEffect(() => {
-    //     if(location.state){
-    //       console.log(location.state,"esto lo de location");
-    //       const infoPersonal1 = location.state;
-    //       const infoPosts1 = location.state.posts;
-    //       console.log(infoPersonal1);
-    //       setDataUser1(infoPersonal1)
-    //       setDataPost1(infoPosts1)
-    //     }
-    //   }, [])
+const PostComment = ({ dataPost }) => {
+    const [comentario, setcomentario] = useState([])
+    if (!Array.isArray(dataPost) || dataPost.length === 0) {
+        console.error("El array de comentarios está vacío o no es un array.");
+        return null;
+    }
+    const publicacion = dataPost[0];
+    console.log(publicacion, "esto es el comentario");
+    console.log(publicacion.comentarios);
+    
 
   return (
     <section className="container__comment">
         <article className="post__description">
-            <h1 className='title__post'>{dataPost.titulo? dataPost.titulo:"Titulo Publicación"}</h1>
+            <h1 className='title__post'>{publicacion? publicacion.titulo:"Titulo publicación"}</h1>
             <p className="description">
-            {data.descripcion?data.descripcion:"Descripcion del proyecto"}
+            {publicacion? publicacion.descripcion: "publicacion"}
             </p>
         </article>
         <input type="text" className="postSend" placeholder='Comentarios' />
@@ -30,8 +24,10 @@ const PostComment = ({dataPost}) => {
             <div className="comment">
                 <img src="https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg" alt="foto de perfil" className="img__comment" />
                 <div className="container__name--comment">
-                    <span className="name__comment">Juan perez garcia</span>
-                    <span className="comment__check">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi magnam doloremque, nulla ipsam dolore illum?</span>
+          
+                            <span className="name__comment">{comentario?comentario.nombreComentario:"comentario"}</span>
+                            <span className="comment__check">{comentario?comentario.nombreComentario:"descripcion"}</span>
+       
                 </div>
             </div>
         </article>
