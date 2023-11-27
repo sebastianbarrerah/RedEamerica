1895a8d84e0a1199b670f1b404360ed5fb3cd608
an <angierincon326@gmail.com>
2023-11-26 23:27:33 -0500

    home

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,40 +1,34 @@
 import React from "react";
 import sujeto from "../../assets/sujeto.jpg";
 import ImgPost from "../../assets/abuela.jpg";
+import { FaUserAstronaut } from "react-icons/fa";
 import "./Post.scss";
 
 const Post = () => {
   return (
-    <>
-      <section className="secPost">
-        <div className="secPost__ImgNameTxt">
-          <div className="secPost__ImgName">
-            <img className="sujeto" src={sujeto} alt="" />
+    <section>
+      <div className="secPost__ImgNameTxt">
+        <div className="secPost__ImgName">
+          <img className="sujeto" src={sujeto} alt="" />
+          <div className="iconName">
+            <FaUserAstronaut color="yellowgreen" />
             <p className="nombre">Consuelo Correa</p>
           </div>
-          <div className="secPost__ImgTxt">
-            <img src={ImgPost} alt="" />
-          </div>
         </div>
-        <div className="comentariosContainer">
-          <input type="text" placeholder="Comenta" />
+        <div className="secPost__ImgTxt">
+          <img
+            className="publicacion"
+            src="https://live.staticflickr.com/4504/37150341454_cf597bd461_b.jpg"
+            alt=""
+          />
+          <p className="secPost__p">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Laboriosam, aliquam excepturi! Odio omnis deserunt assumenda velit
+            doloribus accusantium amet necessitatibus?
+          </p>
         </div>
-      </section>
-      <section className="secPost">
-        <div className="secPost__ImgNameTxt">
-          <div className="secPost__ImgName">
-            <img className="sujeto" src={sujeto} alt="" />
-            <p className="nombre">Consuelo Correa</p>
-          </div>
-          <div className="secPost__ImgTxt">
-            <img src={ImgPost} alt="" />
-          </div>
-        </div>
-        <div className="comentariosContainer">
-          <input type="text" placeholder="Comenta" />
-        </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
