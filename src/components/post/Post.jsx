9808b39903dd104bce72989ff4d1eb5be1fdf8a2
DaniaAlvarado <dania26alvarado@gmail.com>
2023-11-26 20:38:39 -0500

    home

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -20,6 +20,20 @@ const Post = () => {
           <input type="text" placeholder="Comenta" />
         </div>
       </section>
+      <section className="secPost">
+        <div className="secPost__ImgNameTxt">
+          <div className="secPost__ImgName">
+            <img className="sujeto" src={sujeto} alt="" />
+            <p className="nombre">Consuelo Correa</p>
+          </div>
+          <div className="secPost__ImgTxt">
+            <img src={ImgPost} alt="" />
+          </div>
+        </div>
+        <div className="comentariosContainer">
+          <input type="text" placeholder="Comenta" />
+        </div>
+      </section>
     </>
   );
 };
